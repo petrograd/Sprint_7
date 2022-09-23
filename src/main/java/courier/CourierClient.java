237910b22cd54593cc6c62fb6ad701a2cb8a7108
Petr Merkurev <9662196@mail.ru>
2022-09-23 18:55:13 +0300

    CourierDeleteTest DONE

--- src/main/java/courier/CourierClient.java
+++ src/main/java/courier/CourierClient.java
@@ -7,26 +7,26 @@ public class CourierClient extends BaseClient {
 
     public ValidatableResponse create(Courier courier) {
         return getSpec()
-                .body(courier)
-                .when()
-                .post(Config.COURIER_ENDPOINT)
-                .then().log().all();
+               .body(courier)
+               .when()
+               .post(Config.COURIER_ENDPOINT)
+               .then().log().all();
     }
 
     public ValidatableResponse login(CourierCredentials creds) {
         return getSpec()
-                .body(creds)
-                .when()
-                .post(Config.LOGIN)
-                .then().log().all();
+               .body(creds)
+               .when()
+               .post(Config.LOGIN)
+               .then().log().all();
     }
 
-    public void delete(int courierId) {
-        getSpec()
-                .pathParam("courierId", courierId)
-                .when()
-                .delete(Config.COURIER_ID)
-                .then().log().all();
+    public ValidatableResponse delete(int courierId) {
+       return getSpec()
+               .pathParam("courierId", courierId)
+               .when()
+               .delete(Config.COURIER_ID)
+               .then().log().all();
 
     }
 }
---
+++ src/test/java/courier/CourierDeleteTest.java
@@ -0,0 +1,89 @@
+
+
+package courier;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertTrue;
+
+public class CourierDeleteTest {
+
+    private Courier courier;
+    private CourierClient courierClient;
+    private int courierId = 0;
+
+
+    @Before
+    //create courier
+    public void setup() {
+        courier = Courier.getRandomCourier();
+        courierClient = new CourierClient();
+        boolean isOk = courierClient.create(courier)
+                .extract().path("ok");
+
+        assertTrue(isOk);
+
+        CourierCredentials creds = CourierCredentials.from(courier);
+        courierId = courierClient.login(creds)
+                .extract().path("id");
+    }
+
+    @After
+    public void teardown() {
+        if (courierId != 0)
+            courierClient.delete(courierId);
+    }
+
+    @Test
+    @DisplayName("Существующего курьера удалить можно")
+    @Description("Ожидаемый код ответа: 200.")
+    public void shouldDeleteValidCourier() {
+
+       int expectedStatusCode = 200;
+
+       courierClient.delete(courierId)
+               .assertThat()
+               .statusCode(expectedStatusCode)
+               .and()
+               .body("ok", is(true));
+        courierId = 0;
+    }
+
+    @Test
+    @DisplayName("Ошибка при попытке удалить курьера с несуществующим id")
+    @Description("Ожидаемый код ответа: 404")
+    public void shouldNotDeleteWithWrongId() {
+
+        int expectedStatusCode = 404;
+
+        courierClient.delete(courierId)
+                        .statusCode(200);
+
+        courierClient.delete(courierId)
+                .assertThat()
+                .statusCode(expectedStatusCode)
+                .and()
+                .body("message", is("Курьера с таким id нет."));
+    }
+
+//    @Test
+//    @DisplayName("Удаление курьера. Ошибка в id запроса")
+//    @Description("Ожидаемый код ответа: 500")
+//    public void shouldNotDeleteWithoutId() {
+//
+//        int expectedStatusCode = 500;
+//        courierClient.deleteWithoutId()
+//                .assertThat()
+//                .statusCode(expectedStatusCode);
+//    }
+
+
+
+}
+
